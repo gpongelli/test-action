fc257deeb16438ec9db140d947183ed89451d3e1
gpongelli <gpongelli@users.noreply.github.com>
2023-01-31 22:21:08 +0100

    call script_ok.py

--- script_ok.py
+++ script_ok.py
@@ -1,4 +1,16 @@
 import sys
 
 if __name__ == "__main__":
-    sys.exit(0)
+    # sys.exit(0)
+
+    from concurrent.futures import ThreadPoolExecutor
+    _d = {'f': 4, 'l':11}
+    _l = ['a,', 'b','c']
+
+    def logall(l):
+        print(f"{l}\n{_d}\n\n")
+
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        results = executor.map(logall, _l)
+
