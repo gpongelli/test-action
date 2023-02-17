9dd8811e918f0cdfe88c36ea6ee64d6b6e1dabf0
Gabriele Pongelli <gpongelli@users.noreply.github.com>
2023-02-17 12:01:26 +0100

    build openssl and install cryptography against it

---
+++ install_openssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+OPENSSL_VERSION="1.1.1t"
+CWD=$(pwd)
+
+mkdir -p /opt/openssl
+
+virtualenv env
+. env/bin/activate
+pip install -U setuptools
+pip install -U wheel pip
+curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar xvf openssl-${OPENSSL_VERSION}.tar.gz
+cd openssl-${OPENSSL_VERSION}
+./config no-shared no-ssl2 no-ssl3 -fPIC --prefix=/opt/openssl
+make && make install
+cd ..
